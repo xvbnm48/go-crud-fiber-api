43d27d28616e0ec79b80d97a614360cadad22966
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-08-13 18:10:19 +0700

    create,update, show, get book by id controller

--- controllers/bookControllers/bookControllers.go
+++ controllers/bookControllers/bookControllers.go
@@ -1,17 +1,75 @@
 package bookControllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/xvbnm48/go-crud-fiber-api/models"
+	"gorm.io/gorm"
+)
 
 func Index(c *fiber.Ctx) error {
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"message": "Hello World!",
+	var books []models.Book
+	models.DB.Find(&books)
+
+	return c.Status(200).JSON(fiber.Map{
+		"data": books,
 	})
 }
 
-func Show(c *fiber.Ctx) error { return nil }
+func Show(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var book models.Book
+	if err := models.DB.First(&book, id).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+				"message": "Book not found",
+			})
+		}
 
-func Create(c *fiber.Ctx) error { return nil }
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
 
-func Update(c *fiber.Ctx) error { return nil }
+	return c.JSON(book)
+}
+
+func Create(c *fiber.Ctx) error {
+	var book models.Book
+	if err := c.BodyParser(&book); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	if create := models.DB.Create(&book).Error; create != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": create.Error(),
+		})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Book created successfully",
+	})
+
+}
+
+func Update(c *fiber.Ctx) error {
+	id := c.Params("id")
+	var book models.Book
+
+	if err := c.BodyParser(&book); err != nil {
+		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+	if models.DB.Where("id = ?", id).Updates(&book).RowsAffected == 0 {
+		c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"message": "Book not found",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "Book updated successfully",
+	})
+}
 
 func Delete(c *fiber.Ctx) error { return nil }
